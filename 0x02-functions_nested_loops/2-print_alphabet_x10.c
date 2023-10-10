8d2ab383150ad52180586519594ccb59fce2fe6c
NOSSAYBA0 <nossayba.satei@usmba.ac.ma>
2023-10-10 15:55:19 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print the alphabet 10 times
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+
+{
+	char l;
+	int i = 0;
+
+	while (i <= 9 )
+	{
+		for (l ='a'; l <= 'z'; l++)
+		{
+			_putchar(l);
+		}
+		_putchar('\n');
+		i++;
+		
+	}
+
+}	
