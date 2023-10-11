b60f8bf025ba3b021757c4cc81ebdafd7e7cc894
NOSSAYBA0 <nossayba.satei@usmba.ac.ma>
2023-10-10 19:14:16 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,8 +2,7 @@
 /**
  * _islower - Checks for lowercase character
  * @c: The character to be checked
- * Return:
- * 1 for lowercase character or 0 for anything else
+ * Return: 1 for lowercase character or 0 for anything else
  */
 
 int _islower(int l)
