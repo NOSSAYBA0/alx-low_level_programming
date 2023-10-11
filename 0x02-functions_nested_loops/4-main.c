8f538d6bdd3b08512f7bb23fd4b4c7b863c4a710
NOSSAYBA0 <nossayba.satei@usmba.ac.ma>
2023-10-11 12:05:08 -0700

    5-sign.c

--- 0x02-functions_nested_loops/4-main.c
+++ 0x02-functions_nested_loops/4-main.c
@@ -19,4 +19,3 @@ int main(void)
     _putchar(r + '0');
     _putchar('\n');
     return (0);
-}
---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sing - Prints the sign of a number
+ * @n: The number to be cheked
+ * Return: 1 if n positive, -1 if n is negative or 0 for anything else
+ */
+
+int print_sign(int n)
+
+{
+	if (n > 0) 
+	{
+        _putchar(43);
+	return (1);
+	}
+	else if (n < 0)
+	{
+	_putchar(45);
+	return (-1);
+	}
+	else 
+	{
+	_putchar(48);
+	return (0);
+	}
+}
