d09df5ec749a6fe1132ab77f80d7ea6caeb90697
NOSSAYBA0 <nossayba.satei@usmba.ac.ma>
2023-10-17 15:16:01 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,16 +9,15 @@
 void print_rev(char *s)
 
 {
-        int c = 0;
-
-        while (s[c] != '\0')
-        {
-                c++;
-        }
-
-        for (c -= 1; c >= 0; c--)
-        {
-                _putchar(s[c]);
-        }
-        _putchar('\n');
+	int c = 0;
+	
+	while (s[c] != '\0')
+	{
+		c++;
+	}
+	for (c -= 1; c >= 0; c--)
+	{
+		_putchar(s[c]);
+	}
+	_putchar('\n');
 }
