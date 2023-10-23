4b81d2421535f0998fd67a74d497eb76834f8a92
NOSSAYBA0 <nossayba.satei@usmba.ac.ma>
2023-10-23 16:20:11 -0700

    1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _memcpy - main function prototype
+ * @src: Source parameter
+ * @dest: destination parameter
+ * @n: dunction parameter
+ * Return: Copied memory
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int k;
+
+	for (k = 0; k < n; k++)
+	{
+		dest[k] = src[k];
+	}
+	return (dest);
+}
