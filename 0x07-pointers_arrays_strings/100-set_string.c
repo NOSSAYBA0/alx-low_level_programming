5912be97574ba20b22b8fc0f60809662eff94e63
NOSSAYBA0 <nossayba.satei@usmba.ac.ma>
2023-10-23 19:54:57 -0700

    100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - main function
+ * @s: function parameter
+ * @to: function parameter
+ * Return: always 0
+ */
+void set_string(char **s, char *to)
+
+{
+	*s = to;
+}
