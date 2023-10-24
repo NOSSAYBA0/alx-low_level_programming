c25bea74de80394f9e5fb352acdf5c0eb6288a4f
NOSSAYBA0 <nossayba.satei@usmba.ac.ma>
2023-10-23 17:36:32 -0700

    3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strspn - main function
+ * @: fonction parameter
+ * @accept: function parameter
+ * Return: always 0
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+
+	unsigned int _strpn(char *s, char *accept);
+	unsigned int i = 0;
+	int k;
+
+	while (*s)
+	{
+		for (k = 0; accept[k]; k++)
+		{
+			if (*s == accept[k])
+			{
+				i++;
+				break;
+			}
+			else if (accept[k + 1] == '\0')
+				return (i);
+		}
+		s++;
+	}
+	return (i);
+}
