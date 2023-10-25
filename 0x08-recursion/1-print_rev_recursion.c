c8231eb60368a0da9b6ba78124681c019e2679c6
NOSSAYBA0 <nossayba.satei@usmba.ac.ma>
2023-10-25 16:07:47 -0700

    1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - main function
+ * @s: function parameter
+ * Return: voidu
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
