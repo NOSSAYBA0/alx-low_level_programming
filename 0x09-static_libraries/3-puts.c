d829455d2b4d3799e0dfc6d8f2d4b222c9df55f7
NOSSAYBA0 <nossayba.satei@usmba.ac.ma>
2023-11-21 16:07:38 -0800

    libmy.a

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _puts - program prints a string to stdout
+ * @str: the string to be printed
+ * _putchar prints a new line
+ */
+void _puts(char *str)
+{
+	while (*str)
+		_putchar(*str++);
+
+	_putchar('\n');
+}
---
+++ 0x09-static_libraries/4-strpbrk.c
@@ -0,0 +1,24 @@
+#include"main.h"
+
+/**
+ * _strpbrk - Entry point
+ * @s: input
+ * @accept: input
+ * Return: Always 0 (Success)
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int b;
+
+	while (*s)
+	{
+		for (b = 0; accept[b]; b++)
+		{
+			if (*s == accept[b])
+				return (s);
+		}
+		s++;
+	}
+
+	return ('\0');
+}
